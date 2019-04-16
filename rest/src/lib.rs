ce47c48d8a13fa446da34ee482bbf5d53324b43d
Texlo-Dev <richrancy@gmail.com>
2019-04-15 21:15:54 -0400

    [REST] Update documentation example.

--- rest/src/lib.rs
+++ rest/src/lib.rs
@@ -13,8 +13,11 @@
 //!
 //! ## Views
 //! The Client ships with three views for specific endpoints of the Discord API.
+//!
 //! The [`ChannelView`] provides a set of methods for interacting with a Discord channel.
-//! The [`GuildlView`] provides a set of methods for interacting with a Discord guild, or "server".
+//!
+//! The [`GuildView`] provides a set of methods for interacting with a Discord guild, or "server".
+//!
 //! The [`WebhookView`] provides a set of methods for interacting with Discord webhooks.
 //!
 //! [`ChannelView`]: struct.ChannelView.html
@@ -23,25 +26,27 @@
 //! [`RestClient`]: struct.RestClient.html
 //!
 //! Here is a brief example of sending a message to a Discord channel using the ChannelView.
-//! ```rust
+//! ```rust, norun
+//! use tokio::prelude::*;
 //! use spectacles_rest::RestClient;
 //! use spectacles_model::Snowflake;
 //!
 //! fn main() {
-//!     // ~snip~
 //!     // Initialize the Rest Client, with a token.
 //!     let rest = RestClient::new(token, true);
-//!     tokio::run_async(async {
-//!         // We create a Snowflake object from an ID that we provide, passing it to the ChannelView.
-//!         await!(rest.channel(&Snowflake(CHANNEL_ID_HERE)).create_message("Hello World."))
-//!             .expect("Failed to send message to Discord");
-//!     });
+//!     tokio::run(rest.channel(&Snowflake(CHANNEL_ID_HERE)).create_message("Hello World")
+//!         .map(|message| println!("Message Sent: {:?}", message))
+//!         .map_err(|err| {
+//!             eprintln!("Error whilst attempting to send message. {:?}", err);
+//!         })
+//!     );
 //! }
 //! ```
 //! ## Rate Limiting
 //! As mentioned earlier, the library includes an in-memory rate limiter bucket system for preemptively managing Discord Ratelimits.
 //! This is sufficient if you do not plan on accessing the Discord API from a single server.
 //! If you plan to make requests in a distributed fashion, you will need to make use of an external state for keeping track of rate limits.
+//!
 //! The library currently supports using a custom Discord proxy, featured in the Spectacles client, to be used as a central hub for handling requests.
 //! ```rust
 //! use spectacles_rest::RestClient;
@@ -58,9 +63,7 @@
 //! ```toml
 //! [dependencies]
 //! spectacles-rest = "0.1.0"
-//! ```
 //!
-//! ```
 
 #[macro_use]
 extern crate log;
